e38304223daa7e61c6cf1c114b2e9db7f0c5002c
giles <giles.bradshaw@sigyl.com>
2015-05-24 07:51:42 +0100

    common webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,33 @@
 var path = require('path');
+var merge = require('./lib/merge');
  
-module.exports = {
-   entry: path.resolve(__dirname, 'app/main.js'),
-   output: {
-     path: path.resolve(__dirname, 'build'),
-     filename: 'bundle.js',
-   },
+var TARGET = process.env.TARGET;
+var ROOT_PATH = path.resolve(__dirname);
+ 
+var common = {
+  entry: [path.join(ROOT_PATH, 'app/main')],
+  output: {
+    path: path.resolve(ROOT_PATH, 'build'),
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css']
+      }
+    ]
+  },
 };
+
+var mergeConfig = merge.bind(null, common);
+
+if(TARGET === 'build') {
+  module.exports = mergeConfig({});
+}
+
+if(TARGET === 'dev') {
+  module.exports = mergeConfig({
+    entry: ['webpack/hot/dev-server']
+  });
+}
